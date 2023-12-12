edccf8732bda3f93d9caf19898cca4786cd4f1e0
MathijsVanEngelen <m.vanengelen4@student.avans.nl>
2023-12-12 17:05:15 +0100

    begonnen met students tabblad in gui en database connectie

--- Project_Codecademy/src/GUI/CodecademyGUI.java
+++ Project_Codecademy/src/GUI/CodecademyGUI.java
@@ -1,28 +1,29 @@
 package GUI;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class CodecademyGUI extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        
-        Menu menuItem = new Menu("opties");
-
-        MenuItem Students = new MenuItem("Studenten");
-        MenuItem Content = new MenuItem("Content");
-
-        menuItem.getItems().add(Students);
-        menuItem.getItems().add(Content);
-
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(menuItem);
-
-        stage.show();
         stage.setTitle("menu");
+
+        Button studentenButton = new Button("Studenten");
+        Button contentButton = new Button("Content");
+        
+        HBox box = new HBox();
+        box.getChildren().addAll(studentenButton, contentButton);
+
+        Scene scene = new Scene(box, 100, 200);
+
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
---
+++ Project_Codecademy/src/GUI/Students.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import Database.databaseConnection;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+class Students extends Application {
+
+    @Override
+    public void start(Stage studentGUI) throws Exception {
+        databaseConnection databaseConnection = new databaseConnection();
+        databaseConnection.openConnection();
+
+    }
+
+}
