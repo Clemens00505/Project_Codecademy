2cc5e9f3d58462821f693637591f09cd051e58d1
MilAngEr03 <105241065+MilAngEr03@users.noreply.github.com>
2024-01-10 13:40:25 +0100

    Create CodecademyGUI.java

---
+++ Project_Codecademy/src/gui/CodecademyGUI.java
@@ -0,0 +1,61 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class CodecademyGUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Menu");
+
+        Button studentenButton = new Button("Studenten");
+        Button contentButton = new Button("Content");
+        
+        VBox box = new VBox();
+        box.getChildren().addAll(studentenButton, contentButton);
+
+        Scene scene = new Scene(box, 100, 200);
+
+        stage.setScene(scene);
+        stage.show();
+
+
+        //Buttons that open different screens
+        studentenButton.setOnAction((event) -> {
+            StudentGUI studentGUI = new StudentGUI();
+
+            Stage studentStage = new Stage();
+            studentStage.setTitle("Studenten");
+
+            try {
+                studentGUI.start(studentStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //close current stage
+            stage.close();
+        });
+
+        contentButton.setOnAction((event) -> {
+            ContentGUI contentGUI = new ContentGUI();
+
+            Stage contentStage = new Stage();
+            contentStage.setTitle("Content");
+
+            try {
+                contentGUI.start(contentStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            stage.close();
+        });
+    
+
+    }
+}
