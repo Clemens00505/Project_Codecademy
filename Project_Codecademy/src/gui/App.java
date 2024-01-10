316e0ece2e66a6d3fa1aeb54e768af7473176689
MilAngEr03 <105241065+MilAngEr03@users.noreply.github.com>
2024-01-10 13:39:48 +0100

    Create App.java

---
+++ Project_Codecademy/src/gui/App.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javafx.application.Application;
+
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(CodecademyGUI.class);
+
+    }
+}
