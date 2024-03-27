e64046515c15ae5d534593ce9ac364c07664a2ab
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-03-27 17:33:45 +0100

    .

--- Project_Codecademy/src/gui/Main.java
+++ Project_Codecademy/src/gui/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(CertificateGUI.class);
+        Application.launch(CodecademyGUI.class);
 
     }
 }
