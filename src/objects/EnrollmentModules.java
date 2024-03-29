5587ca3f3bdf96826eaa4d3718005a99636c8443
MathijsVanEngelen <m.vanengelen4@student.avans.nl>
2024-03-29 01:00:46 +0100

    begonnen met enrollmentmodulesgui

---
+++ src/objects/EnrollmentModules.java
@@ -0,0 +1,13 @@
+package objects;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EnrollmentModules extends Application {
+
+    @Override
+    public void start(Stage enrollmentModuleStage) throws Exception {
+        
+    }
+
+}
